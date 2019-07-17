5b948cc636c55f005a8a7b14dc68407b1a8d40e0
karolusM <49364741+karolusM@users.noreply.github.com>
2019-07-17 13:18:43 +0200

    Add files via upload

---
+++ app/saludo.php
@@ -0,0 +1,25 @@
+<?php
+    $ip = $_SERVER['SERVER_ADDR'];
+?>
+<html>
+	<head>
+		<title>Ejemplo base <?= $ip ?></title>		
+	</head>
+	<body>
+		<?php
+		echo "Hola!<br><br>Hoy es: ".date("d/m/Y h:i");
+		echo "<br>";
+		echo "Server IP: $ip<br>";
+		echo "<br>Headers<br><br>";
+
+		foreach (getallheaders() as $nombre => $valor) {
+			echo "$nombre: $valor<br>";
+		}			
+		?>
+	</body>
+</html>
+
+
+
+
+	
